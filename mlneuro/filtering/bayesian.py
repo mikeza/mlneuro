9984973e26f8013b538f0931557dfbb38dd26721
Michael Adkins <adkin099@umn.edu>
2018-08-08 14:41:41 -0500

    Updated documentations, added multithreading, fixed kernel call in TransitionInformedBayesian

--- mlneuro/filtering/bayesian.py
+++ mlneuro/filtering/bayesian.py
@@ -3,22 +3,33 @@ import numpy as np
 from numba import jit
 from sklearn.base import BaseEstimator
 
-from ..common.math import tiny_epsilon
+from ..common.math import tiny_epsilon, _gaussian_pdf
 from ..common.bins import bin_centers_from_edges, linearized_bin_grid, binned_data
+from ..utils.parallel import spawn_threads
 
 class TransitionInformedBayesian(BaseEstimator):
 
     def __init__(self, transition_obs, bin_edges, recursive_update_prop=0, 
-        transition_model='indiscriminate', propogation_factor=1, 
-        transition_model_kwargs={}, transition_model_func=None, **kwargs):
+        transition_model='indiscriminate', propogation_factor=1, n_jobs=-1, transition_model_func=None, 
+        **transition_model_kwargs):
         """
-            :param transition_obs: A numpy array of timestamped observations for the transition matrix creation
-            :param bin_edges: A numpy array defining the bin_edges for each dimension of the estimate. Used to map observations into estimate bins.
-                Additionally, a callable function can be passed instead if bin edges are not yet defined
-            :param transition_model='indiscriminate': 
-            :param propogation_factor=1: 
-            :param transition_model_kwargs=None: 
-            :param transition_model_func=None:  A function that takes the observations and bin edges and returns a transition matrix
+        transition_obs: array-like shape=[n_times, n_dims]
+            Timestamped observations for the transition matrix creation
+        bin_edges: array-like shape [n_dims, n_bins_dim]
+            Defines bin edges for each dimension of the estimate. Used to map observations into estimate bins.
+                Additionally, a callable function can be passed instead if bin edges are defined at filter
+                construction time.
+        transition_model: string (optional='indiscriminate')
+            The transition model to use for constructing a transition matrix:
+                'indiscriminate': a kernel based spread based on mean velocity
+                'directional': bin transition based in the forward direction
+                'bidirectional': bin transition based in the sum of forward and backward directions
+                'custom': must define transition_model_func, allows a custom matrix function
+        propogation_factor, float range [0,1] (optional=1)
+        transition_model_func: function (optional=None)
+            Function that takes the observations and bin edges and returns a transition matrix
+
+        Additional keyword arguments are passed to the transition model function
         """
 
         TRANSITION_MODELS = {'indiscriminate': self._indiscriminate_kernel_transition_matrix,
@@ -47,6 +58,7 @@ class TransitionInformedBayesian(BaseEstimator):
         self.transition_model_kwargs = transition_model_kwargs
         self.propogation_factor = propogation_factor
         self.recursive_update_prop = recursive_update_prop
+        self.n_jobs = n_jobs
 
     def fit(self, T, y_proba):
 
@@ -61,7 +73,7 @@ class TransitionInformedBayesian(BaseEstimator):
             posterior = self.y_proba.copy()
             estimates = self.y_proba
         else:
-            posterior = self._align_to_test_times(T_test, self.M, self.y_proba)
+            posterior = self._align_to_test_times(T_test, self.T_fit, self.y_proba)
             estimates = posterior.copy()
 
         epsilon = tiny_epsilon(posterior.dtype)
@@ -70,15 +82,24 @@ class TransitionInformedBayesian(BaseEstimator):
         recursive_update_prop = self.recursive_update_prop
         M = self.M
 
-        @jit(nopython=True)
-        def _inner_worker(posterior):
-            for i in range(1, T_test.shape[0]):
+        @jit(nogil=True)
+        def _inner_worker(posterior, i_start, i_end):
+            if i_start == 0:
+                i_start += 1
+
+            for i in range(i_start, i_end):
                 curEstimate = posterior[i, :]
 
                 # Get the 'previous' y_proba as a mix between the transformed
                 #   posterior at that time and the original value
-                prevEstimate = posterior[i - 1, :] * recursive_update_prop + \
-                    estimates[i - 1, :] * (1 - recursive_update_prop)
+                if recursive_update_prop == 1:
+                    prevEstimate = posterior[i - 1, :]
+                elif recursive_update_prop == 0:
+                    prevEstimate = estimates[i - 1, :]
+                else:
+                    prevEstimate = (posterior[i - 1, :] * recursive_update_prop +
+                        estimates[i - 1, :] * (1 - recursive_update_prop))
+
                 prevEstimate /= np.nansum(prevEstimate)
 
                 # Apply a transformation
@@ -88,28 +109,28 @@ class TransitionInformedBayesian(BaseEstimator):
                 posterior[i, :] += epsilon
                 posterior[i, :] /= np.nansum(posterior[i, :])
 
-        _inner_worker(posterior)
+        spawn_threads(self.n_jobs, T_test, target=_inner_worker, args=([posterior]))
 
         return posterior
 
-    def _align_to_test_times(self, T_test, data_times, data):
-        insert_indices = np.searchsorted(T_test, data_times)
+    def _align_to_test_times(self, T_test, T_fit, y_proba):
+        insert_indices = np.searchsorted(T_test, T_fit)
 
-        aligned_data = np.ones((T_test.shape[0], data.shape[1]))
+        aligned_y = np.ones((T_test.shape[0], y_proba.shape[1]))
 
-        for i in range(data.shape[0]):
+        for i in range(y_proba.shape[0]):
             insert_idx = insert_indices[i]
-            if insert_idx >= aligned_data.shape[0]:
+            if insert_idx >= aligned_y.shape[0]:
                 continue  # Skip indices beyond the end of the new array
             
             # Note: We are multiplying rather than assigning so that spikes that share a position will combine information
-            # and positions without spikes will be uniform since aligned_data was initialized with ones
-            aligned_data[insert_idx] *= data[i]
-            aligned_data[insert_idx] /= np.nansum(aligned_data[insert_idx])
+            # and positions without spikes will be uniform since aligned_y was initialized with ones
+            aligned_y[insert_idx] *= y_proba[i]
+            aligned_y[insert_idx] /= np.nansum(aligned_y[insert_idx])
 
         # Force normalization
-        aligned_data /= np.nansum(aligned_data, axis=1)[:, np.newaxis]
-        return aligned_data
+        aligned_y /= np.nansum(aligned_y, axis=1)[:, np.newaxis]
+        return aligned_y
 
     def _init_transition_matrix(self):
         bin_edges = self.bin_edges() if callable(self.bin_edges) else self.bin_edges
@@ -142,12 +163,10 @@ class TransitionInformedBayesian(BaseEstimator):
         self._transition_matrix_velocity = v
 
         # Get the squared distance from bin -> bin for all bins
-        sq_dists = bin_distances(bin_grid, return_squared=True)
+        dists = bin_distances(bin_grid, return_squared=True)
 
         # Apply a gaussian kernel to every distance
-        dist_matrix_gaussian = np.vectorize((lambda dist: sqd_gaussian_kernel(dist, v)))
-
-        return dist_matrix_gaussian(sq_dists)
+        return _gaussian_pdf(dists, std_deviation=v)
 
     def _directional_transition_matrix(self, observations, bin_edges):
         binned_obs = binned_data(observations[:,1:], bin_edges, flat=True)
