4a8e07e1f9fe0b859c2d66c1e4746dcc1cb09d3f
Michael Adkins <adkin099@umn.edu>
2018-08-14 13:49:39 -0500

    Added datasets with git LFS

--- mlneuro/datasets/base.py
+++ mlneuro/datasets/base.py
@@ -15,3 +15,18 @@ def load_restaurant_row():
 	"""
 	RR_FILE = 'RestaurantRowExampleDay.pickle'
 	return load_array_dict(os.path.join(data_path(), RR_FILE))
+
+
+def load_restaurant_row_time_out(which='rr'):
+	"""Load an example day with Restaurant Row and Time Out data from the Redish lab
+	"""
+	RR_FILE = 'RestaurantRowTimeOutExampleDay_RR.pickle'
+	TO_FILE = 'RestaurantRowTimeOutExampleDay_TO.pickle'
+
+	if which == 'to':
+		return load_array_dict(os.path.join(data_path(), RR_FILE))
+	elif which == 'rr':
+		return load_array_dict(os.path.join(data_path(), TO_FILE))
+	else:
+		raise ValueError('Unknown option for which data to load.'
+						 'Please specify `rr` for restaurant row or `to` for time out')
